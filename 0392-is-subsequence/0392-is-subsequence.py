2e49c84a7040cfafd4744aef58d5b562d4da5cb7
Shamueel <58599636+Shamueel@users.noreply.github.com>
2022-12-02 15:20:04 +0500

    Time: 57 ms (61.01%), Space: 14 MB (11.70%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,15 +4,15 @@ class Solution:
         count = 0
         for i in range(len(s)):
             
-            print("first loop",s[i])
+            #print("first loop",s[i])
             for j in range(x,len(t)):
-                print('x =',x)
-                print(t[j])
+               # print('x =',x)
+                #print(t[j])
                 if s[i] == t[j] :
-                    print('j=',j)
+                 #   print('j=',j)
                     x = j+1
                     count += 1
                     break
-        print("count",count)
+       # print("count",count)
         if count == len(s):
             return True
