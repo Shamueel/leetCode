6acad3e92fa7da4125f8a761d64ceb964a8b4cda
Shamueel <58599636+Shamueel@users.noreply.github.com>
2023-03-12 00:36:27 +0500

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,22 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        #time O(nlogn) 
+        '''
+        print(nums)
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return True
+        '''
+        #time O(n)
+        pmap = {}
+        for i,num in enumerate(nums):
+            if nums[i] in pmap:
+                return True
+            else:
+                pmap[num]=i
+        #print(pmap)
+        if 6 in pmap:
+            print("6 in here")
+            return False
+        
